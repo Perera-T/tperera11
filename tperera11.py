9b7a7621a51cfa1f18198483bce9f899d5e1934e
tp <you@example.com>
2023-11-10 02:35:59 +0000

    adding a python program

---
+++ tperera11.py
@@ -0,0 +1,9 @@
+#Thisara Perera
+#CSC-321
+#Lab #11
+
+num=int(input("Please enter a number: "))
+num**=2
+print("Your number squared equals: ",num)
+print("Don't be afraid especially if you've never tried")
+
